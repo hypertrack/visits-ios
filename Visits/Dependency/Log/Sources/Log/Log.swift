37044cbc9bf76e567e52da7f9b5f8e08217fb127
Eugene Tulushev <eugene@tulushev.com>
2020-12-01 18:06:59 +0200

    Improve log output for large state structures

--- Visits/Dependency/Log/Sources/Log/Log.swift
+++ Visits/Dependency/Log/Sources/Log/Log.swift
@@ -17,7 +17,7 @@ public func logEffect(_ message: String) {
 public func logAction(_ message: String) {
   #if DEBUG
     if #available(iOS 14, *) {
-      loggerAction.log("🚀 \(message)")
+      logInChunks({ loggerAction.log("\($0)") }, message: "🚀 " + message)
     }
   #endif
 }
@@ -26,5 +26,27 @@ public func logEffect<E>(_ message: String, failureType: E.Type) -> Effect<Void,
   .result {
     logEffect(message)
     return .success(())
+  }
+}
+
+
+func logInChunks(_ log: (String) -> Void, message: String) {
+  // Circumventing 1024 byte storage limit for dynamic content in os_log
+  let bytes: [UInt8] = message.utf8.map { UInt8($0) }
+  // Value below proved to work without truncation while running without a
+  // debugger attached (with debugger value of 1024 worked correctly)
+  let chunkSize = 900
+
+  if bytes.count > chunkSize {
+    let kibibyteChunks = stride(from: 0, to: bytes.count, by: chunkSize).map {
+      Array(bytes[$0 ..< min($0 + chunkSize, bytes.count)])
+    }
+    let stringChunks = kibibyteChunks
+      .compactMap { String(data: Data($0), encoding: .utf8) }
+    for chunk in stringChunks {
+      log(chunk)
+    }
+  } else {
+    log(message)
   }
 }
