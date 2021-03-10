92f764657f8267e6f68e3e48a7c9e71b58ac5e57
Eugene Tulushev <eugene@tulushev.com>
2021-03-10 17:20:42 +0200

    Update Composable Architecture to 0.16.0

--- Visits/Modules/Package.swift
+++ Visits/Modules/Package.swift
@@ -13,7 +13,7 @@ let package = Package(
   ],
   dependencies: [
     .package(name: "Branch",     url: "https://github.com/BranchMetrics/ios-branch-deep-linking-attribution", .exact("1.38.0")),
-    .package(                    url: "https://github.com/pointfreeco/swift-composable-architecture",         .exact("0.15.0")),
+    .package(                    url: "https://github.com/pointfreeco/swift-composable-architecture",         .exact("0.16.0")),
     .package(name: "HyperTrack", url: "https://github.com/hypertrack/sdk-ios",                                .exact("4.7.0")),
     .package(                    url: "https://github.com/pointfreeco/swift-nonempty",
                                                                                                               .revision("b4f37767336e4bb98bffa3e05fad579d91c2b2d4")),
