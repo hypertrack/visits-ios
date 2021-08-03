5d3f8d495abdf11c734fd8b010c97e9313fc9ba0
Eugene Tulushev <eugene@tulushev.com>
2021-08-04 00:57:09 +0300

    Limit to only 15 geofences to speed up the request

--- Visits/Modules/Sources/APIEnvironmentLive/Places.swift
+++ Visits/Modules/Sources/APIEnvironmentLive/Places.swift
@@ -40,7 +40,8 @@ func geofencesRequest(auth token: Token.Value, deviceID: DeviceID, paginationTok
   components.queryItems = [
     URLQueryItem(name: "include_archived", value: "false"),
     URLQueryItem(name: "sort_nearest", value: "true"),
-    URLQueryItem(name: "include_markers", value: "true")
+    URLQueryItem(name: "include_markers", value: "true"),
+    URLQueryItem(name: "limit", value: "15")
   ]
   if let paginationToken = paginationToken, let queryItems = components.queryItems {
     components.queryItems = queryItems + [
