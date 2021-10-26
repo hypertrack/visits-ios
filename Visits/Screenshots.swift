f8651636d2c9a9edbf37cbbd2a61267e0bd0d69b
Eugene Tulushev <eugene@tulushev.com>
2021-10-26 21:05:03 +0300

    Update screenshots

--- Visits/Screenshots.swift
+++ Visits/Screenshots.swift
@@ -16,18 +16,24 @@ extension AppState {
   
   static let orderScreenshot = Self.operational(
     operational_ |> \.flow *< .main(
-      main |> \.selectedOrderId *< .some(notSent.id)
+      main |> \.selectedOrderId *< .some(ongoing.id)
            <> \.tab *< .orders
     )
   )
   
   static let placesScreenshot = Self.operational(
     operational_ |> \.flow *< .main(
-      main |> \.places *< .init(
-        places: [cityHall, artsCenter],
-        requestedAt: Date(),
-        driveDistancesForDaysWithVisits: NonEmptyArray(rawValue: [UInt?](repeating: nil, count: 60))!
-      )
+      main |> \.places *< .some(placesSummary)
+           <> \.history *< history
+           <> \.tab *< .places
+    )
+  )
+
+  static let visitsScreenshot = Self.operational(
+    operational_ |> \.flow *< .main(
+      main |> \.places *< .some(placesSummary)
+           <> \.placesPresentation *< .byVisit
+           <> \.history *< history
            <> \.tab *< .places
     )
   )
@@ -35,41 +41,7 @@ extension AppState {
   static let mapScreenshot = Self.operational(
     operational_ |> \.flow *< .main(
       main |> \.trip *< .some(trip)
-           <> \.history *< History(
-                             coordinates: [
-                               Coordinate(latitude: 37.76477793772538, longitude: -122.41957068443297)!,
-                               Coordinate(latitude: 37.76477793772538, longitude: -122.4196484684944)!,
-                               Coordinate(latitude: 37.77180875405714, longitude: -122.42035388946533)!,
-                               Coordinate(latitude: 37.7730130005326, longitude: -122.42180228233337)!,
-                               Coordinate(latitude: 37.773962814902774, longitude: -122.42069721221922)!,
-                               Coordinate(latitude: 37.776659542769934, longitude: -122.42124438285828)!,
-                               Coordinate(latitude: 37.77676978426915, longitude: -122.42056846618652)!,
-                               Coordinate(latitude: 37.77657474150483, longitude: -122.42050409317015)!,
-                               Coordinate(latitude: 37.77662562227558, longitude: -122.42010712623598)!,
-                               Coordinate(latitude: 37.77690546588866, longitude: -122.42021441459654)!,
-                               Coordinate(latitude: 37.77686306540933, longitude: -122.42057919502257)!,
-                               Coordinate(latitude: 37.77676978426915, longitude: -122.4204933643341)!,
-                               Coordinate(latitude: 37.77671890359764, longitude: -122.42127656936647)!,
-                               Coordinate(latitude: 37.77894914015784, longitude: -122.42174863815306)!,
-                               Coordinate(latitude: 37.77886434151916, longitude: -122.42224216461183)!,
-                               Coordinate(latitude: 37.77883042203647, longitude: -122.4221885204315)!,
-                               Coordinate(latitude: 37.77883042203647, longitude: -122.42229580879211)!,
-                               Coordinate(latitude: 37.779000019294344, longitude: -122.42170572280882)!,
-                               Coordinate(latitude: 37.77989039851448, longitude: -122.42190957069396)!,
-                               Coordinate(latitude: 37.780085432530804, longitude: -122.42036461830139)!,
-                               Coordinate(latitude: 37.774929577713245, longitude: -122.41936683654784)!,
-                               Coordinate(latitude: 37.77681218480201, longitude: -122.41704940795898)!,
-                               Coordinate(latitude: 37.77671890359764, longitude: -122.41700649261476)!,
-                               Coordinate(latitude: 37.77682066490566, longitude: -122.41694211959839)!,
-                               Coordinate(latitude: 37.77686306540933, longitude: -122.41694211959839)!,
-                               Coordinate(latitude: 37.77739730967186, longitude: -122.41634130477905)!,
-                               Coordinate(latitude: 37.78331613854221, longitude: -122.41753220558167)!,
-                               Coordinate(latitude: 37.78321438617593, longitude: -122.41843342781067)!,
-                               Coordinate(latitude: 37.78309567490489, longitude: -122.41842806339264)!,
-                               Coordinate(latitude: 37.783019360415665, longitude: -122.4183851480484)!,
-                               Coordinate(latitude: 37.78315503056424, longitude: -122.41850852966309)!
-                             ]
-                           )
+           <> \.history *< history
            <> \.tab *< .map
     )
   )
@@ -98,8 +70,43 @@ extension AppState {
   )
 }
 
+private let history = History(
+  coordinates: [
+    Coordinate(latitude: 37.76477793772538, longitude: -122.41957068443297)!,
+    Coordinate(latitude: 37.76477793772538, longitude: -122.4196484684944)!,
+    Coordinate(latitude: 37.77180875405714, longitude: -122.42035388946533)!,
+    Coordinate(latitude: 37.7730130005326, longitude: -122.42180228233337)!,
+    Coordinate(latitude: 37.773962814902774, longitude: -122.42069721221922)!,
+    Coordinate(latitude: 37.776659542769934, longitude: -122.42124438285828)!,
+    Coordinate(latitude: 37.77676978426915, longitude: -122.42056846618652)!,
+    Coordinate(latitude: 37.77657474150483, longitude: -122.42050409317015)!,
+    Coordinate(latitude: 37.77662562227558, longitude: -122.42010712623598)!,
+    Coordinate(latitude: 37.77690546588866, longitude: -122.42021441459654)!,
+    Coordinate(latitude: 37.77686306540933, longitude: -122.42057919502257)!,
+    Coordinate(latitude: 37.77676978426915, longitude: -122.4204933643341)!,
+    Coordinate(latitude: 37.77671890359764, longitude: -122.42127656936647)!,
+    Coordinate(latitude: 37.77894914015784, longitude: -122.42174863815306)!,
+    Coordinate(latitude: 37.77886434151916, longitude: -122.42224216461183)!,
+    Coordinate(latitude: 37.77883042203647, longitude: -122.4221885204315)!,
+    Coordinate(latitude: 37.77883042203647, longitude: -122.42229580879211)!,
+    Coordinate(latitude: 37.779000019294344, longitude: -122.42170572280882)!,
+    Coordinate(latitude: 37.77989039851448, longitude: -122.42190957069396)!,
+    Coordinate(latitude: 37.780085432530804, longitude: -122.42036461830139)!,
+    Coordinate(latitude: 37.774929577713245, longitude: -122.41936683654784)!,
+    Coordinate(latitude: 37.77681218480201, longitude: -122.41704940795898)!,
+    Coordinate(latitude: 37.77671890359764, longitude: -122.41700649261476)!,
+    Coordinate(latitude: 37.77682066490566, longitude: -122.41694211959839)!,
+    Coordinate(latitude: 37.77686306540933, longitude: -122.41694211959839)!,
+    Coordinate(latitude: 37.77739730967186, longitude: -122.41634130477905)!,
+    Coordinate(latitude: 37.78331613854221, longitude: -122.41753220558167)!,
+    Coordinate(latitude: 37.78321438617593, longitude: -122.41843342781067)!,
+    Coordinate(latitude: 37.78309567490489, longitude: -122.41842806339264)!,
+    Coordinate(latitude: 37.783019360415665, longitude: -122.4183851480484)!,
+    Coordinate(latitude: 37.78315503056424, longitude: -122.41850852966309)!
+  ]
+)
 
-private let notSent = Order(
+private let ongoing = Order(
   id: Order.ID(rawValue: "ID4"),
   createdAt: Calendar.current.date(bySettingHour: 9, minute: 37, second: 0, of: Date())!,
   location: Coordinate(latitude: 37.780592, longitude: -122.413322)!,
@@ -116,7 +123,7 @@ private let notSent = Order(
   ]
 )
 
-private let entered = Order(
+private let visited = Order(
   id: Order.ID(rawValue: "ID7"),
   createdAt: Calendar.current.date(bySettingHour: 9, minute: 40, second: 0, of: Date())!,
   location: Coordinate(latitude: 37.778655, longitude: -122.422231)!,
@@ -129,7 +136,7 @@ private let entered = Order(
   visited: .entered(Date())
 )
 
-private let checkedOut1 = Order(
+private let completed = Order(
   id: Order.ID(rawValue: "ID1"),
   createdAt: Calendar.current.date(bySettingHour: 9, minute: 35, second: 0, of: Date())!,
   location: Coordinate(latitude: 37.776692, longitude: -122.416557)!,
@@ -142,7 +149,7 @@ private let checkedOut1 = Order(
   visited: .entered(Date())
 )
 
-private let checkedOut2 = Order(
+private let snoozed = Order(
   id: Order.ID(rawValue: "ID2"),
   createdAt: Calendar.current.date(bySettingHour: 9, minute: 36, second: 0, of: Date())!,
   location: Coordinate(latitude: 37.776753, longitude: -122.420371)!,
@@ -150,20 +157,7 @@ private let checkedOut2 = Order(
     street: Street(rawValue: "275 Hayes St"),
     fullAddress: FullAddress(rawValue: "275 Hayes St, San Francisco, CA  94102, United States")
   ),
-  status: .completed(Date()),
-  note: nil,
-  visited: .entered(Date())
-)
-
-private let checkedOut3 = Order(
-  id: Order.ID(rawValue: "ID5"),
-  createdAt: Calendar.current.date(bySettingHour: 9, minute: 38, second: 0, of: Date())!,
-  location: Coordinate(latitude: 37.783049, longitude: -122.418242)!,
-  address: .init(
-    street: Street(rawValue: "601 Eddy St"),
-    fullAddress: FullAddress(rawValue: "601 Eddy St, San Francisco, CA  94109, United States")
-  ),
-  status: .completed(Date()),
+  status: .snoozed,
   note: nil,
   visited: .entered(Date())
 )
@@ -181,9 +175,22 @@ private let canceled = Order(
   visited: .entered(Date())
 )
 
-private let orders = IdentifiedArrayOf<Order>(uniqueElements: [notSent, entered, checkedOut1, checkedOut2, checkedOut3, canceled])
+private let orders = IdentifiedArrayOf<Order>(
+  uniqueElements: [
+    visited,
+    ongoing,
+    completed,
+    snoozed,
+    canceled
+  ]
+)
 
-private let trip = Trip(id: "SADFQSF123", createdAt: Calendar.current.date(bySettingHour: 9, minute: 38, second: 0, of: Date())!, status: .active, orders: orders.elements, metadata: ["Name": "VIP Delivery"])
+private let trip = Trip(
+  id: "SADFQSF123",
+  createdAt: Calendar.current.date(bySettingHour: 9, minute: 38, second: 0, of: Date())!,
+  status: .active, orders: orders.elements,
+  metadata: ["Name": "VIP Delivery"]
+)
 
 private let cityHall = Place(
   id: "1",
@@ -237,6 +244,12 @@ private let artsCenter = Place(
   ]
 )
 
+private let placesSummary = PlacesSummary(
+  places: [cityHall, artsCenter],
+  requestedAt: Date(),
+  driveDistancesForDaysWithVisits: NonEmptyArray(rawValue: [UInt?](repeating: 12345, count: 60))!
+)
+
 private let publishableKey = PublishableKey(rawValue: "Key")
 private let deviceID = DeviceID(rawValue: "UNIQUE-ID")
 private let permissions = Permissions(
@@ -269,5 +282,6 @@ private let main = MainState(
   places: nil,
   tab: .orders,
   publishableKey: publishableKey,
-  profile: .init(name: "User", metadata: ["email": "user@company.com"])
+  profile: .init(name: "User", metadata: ["email": "user@company.com"]),
+  integrationStatus: .integrated(.notRefreshing)
 )
